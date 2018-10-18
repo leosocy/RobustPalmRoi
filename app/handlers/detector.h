dc0fb7cd12b5fbabbe2cb6b39ec1c325563de4e6
leosocy <leosocy@gmail.com>
2018-10-18 22:18:53 +0800

    [Handler] peak valley detector

--- app/handlers/detector.h
+++ app/handlers/detector.h
@@ -5,6 +5,7 @@
 #ifndef ROBUST_PALM_ROI_APP_HANDLERS_DETECTOR_H_
 #define ROBUST_PALM_ROI_APP_HANDLERS_DETECTOR_H_
 
+#include <vector>
 #include "handlers/handler.h"
 
 namespace rpr {
@@ -15,6 +16,7 @@ class Detector : public Handler {
  protected:
   bool ImageOneContour(const cv::Mat& src);
   virtual Status Detect(const cv::Mat& orig, cv::Mat* res) = 0;
+  std::vector<cv::Point> palm_contour_;
 };
 
 inline Status Detector::Handle(const cv::Mat& orig, cv::Mat* res) {
@@ -28,7 +30,19 @@ inline Status Detector::Handle(const cv::Mat& orig, cv::Mat* res) {
 
 class PeakValleyDetector : public Detector {
  private:
+  enum PalmSide {
+    LEFT = 1,
+    RIGHT = -1
+  };
+
   virtual Status Detect(const cv::Mat& orig, cv::Mat* res);
+  void FindHalfSideInflectionPoints(PalmSide side);
+  size_t FindNextInflectionPoint(const std::vector<cv::Point>& contour,
+                                 size_t from_index, size_t to_index, int step, bool is_maximum,
+                                  cv::Point* point);
+
+  std::vector<cv::Point> peaks_;    //  order is from left to right
+  std::vector<cv::Point> valleys_;  //  order is from left to right
 };
 
 }   // namespace rpr
