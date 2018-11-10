469367b9b93e323ff6fa55916a8feff8a7b550c2
leosocy <leosocy@gmail.com>
2018-11-10 15:33:35 +0800

    fix test fail

--- test/test_controller/test_c_api.cpp
+++ test/test_controller/test_c_api.cpp
@@ -29,10 +29,15 @@ TEST_F(CApiTestFixture, test_init_chain) {
 
 TEST_F(CApiTestFixture, test_chain_process_base64) {
   void* chain = init_chain(TEST_CONFIG_YAML_FILE);
-  char roi_base64[1024 * 1024];   // 1 MB can store about 768 KB of image.
+  unsigned long roi_base64_max_size = 1024 * 1024;
+  char roi_base64[roi_base64_max_size];   // 1 MB can store about 768 KB of image.
   char status[128];
-  chain_process_base64(chain, perfect_palm_base64_.c_str(), roi_base64, status);
+  chain_process_base64(chain, perfect_palm_base64_.c_str(), roi_base64, roi_base64_max_size, status);
   EXPECT_EQ(status[0], '\0');
+
+  roi_base64_max_size = 10;
+  chain_process_base64(chain, perfect_palm_base64_.c_str(), roi_base64, roi_base64_max_size, status);
+  EXPECT_NE(status[0], '\0');
 }
 
 TEST_F(CApiTestFixture, test_chain_process_bytes) {
@@ -41,13 +46,19 @@ TEST_F(CApiTestFixture, test_chain_process_bytes) {
   for (size_t i = 0; i < perfect_palm_bytes_.size(); ++i) {
     *(plam_bytes + i) = perfect_palm_bytes_[i];
   }
-  char roi_bytes[1024 * 1024];   // 1 MB can store about 768 KB of image.
+  unsigned long roi_bytes_max_size = 1024 * 1024;
+  char roi_bytes[roi_bytes_max_size];   // 1 MB can store about 768 KB of image.
   unsigned long roi_bytes_size = 0;
   char status[128];
   chain_process_bytes(chain, plam_bytes, perfect_palm_bytes_.size(),
-                      roi_bytes, &roi_bytes_size, status);
+                      roi_bytes, roi_bytes_max_size, &roi_bytes_size, status);
   EXPECT_EQ(status[0], '\0');
   EXPECT_GT(roi_bytes_size, 0);
+
+  roi_bytes_max_size = 10;
+  chain_process_bytes(chain, plam_bytes, perfect_palm_bytes_.size(),
+                      roi_bytes, roi_bytes_max_size, &roi_bytes_size, status);
+  EXPECT_NE(status[0], '\0');
 }
 
 }
